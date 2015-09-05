2aea720a4fe3b9e4388a81aec20948f5ade05f62
daniel fernandez <a>
2015-09-05 12:51:00 +0200

    remove events

--- 2015/javascript/events.js
+++ 2015/javascript/events.js
@@ -2,22 +2,29 @@ function Events(){
     this.keys_down = {}
     this.keys_up = {}
     this.last_key = null
+    this.key_down = null
+    this.key_up   = null
 }
 
 Events.prototype.enableInputs = function(){
     var that = this
-    addEventListener("keydown", function (e) {
+    addEventListener("keydown", this.key_down = function (e) {
         console.log(e.keyCode)
 	    that.keys_down[e.keyCode] = true 
 	    that.last_key = e.keyCode
 	    delete that.keys_up[e.keyCode] 
 	    }, false) 
 
-    addEventListener("keyup", function (e) {
+    addEventListener("keyup", this.key_up = function (e) {
 	    that.keys_up[e.keyCode] = true 
 	    that.last_key = e.keyCode
 	    delete that.keys_down[e.keyCode] 
 	    }, false)
+}
+
+Events.prototype.removeInputs = function(){
+    removeEventListener("keydown", this.key_down)
+    removeEventListener("keyup", this.key_up)
 }
 
 Events.prototype.addKeyDown = function(e){
